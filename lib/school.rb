2ff6bbe4fad9c7ccdf4944a57e0ea18400c0872a
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-30 19:03:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,21 @@
 class School
-  def name
-end 
+  attr_accessor :roster, :student_name, :school_name 
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = Hash.new{ |h, k| h[k] = [] }  
+  end
+  def add_student(student_name, grade)
+      @roster[grade] << student_name
+  end
+  def grade(grade)
+    @grade=grade
+  end 
+  def grade (level) 
+      @roster.select! {|grade| grade==level}.values.flatten    
+  end 
+  def sort  #sort by grade
+   @roster.each do |grade, names|
+     names.sort!
+  end
+end
+end
