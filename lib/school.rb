064c523ce8ad28d7dff1dc01360556b1d54751e4
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-30 19:06:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,7 +2,7 @@ class School
   attr_accessor :roster, :student_name, :school_name 
   def initialize(school_name)
     @school_name = school_name
-    @roster = Hash.new{ |h, k| h[k] = [] }  
+    @roster = Hash.new{ |k, v| k[v] = [] }  
   end
   def add_student(student_name, grade)
       @roster[grade] << student_name
